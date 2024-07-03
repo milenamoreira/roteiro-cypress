8d583226af77c01244c8b858c0e68ec644877d17
milenamoreira <milenacomoreira@gmail.com>
2024-07-02 22:11:43 -0300

    Add 3 test cases

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -56,4 +56,73 @@ describe('template spec', () => {
     cy.get('.todo-list li')
       .should('have.length', 2);
   });
+
+  it('Marcar e desmarcar uma tarefa individualmente', () => {
+    cy.visit('http://127.0.0.1:7001');
+
+     cy.get('.new-todo')
+     .type('TP2 de Engenharia de Software{enter}');
+
+    cy.get('.todo-list li .toggle')
+      .first()
+      .click();
+
+    cy.contains('Completed').click();
+    cy.get('.todo-list li')
+      .should('have.length', 1)
+
+    cy.get('.todo-list li .toggle')
+      .first()
+      .click();
+
+    cy.contains('Active').click();
+    cy.get('.todo-list li')
+      .should('have.length', 1);
+
+ });
+
+ it('Marcar e desmarcar todas as tarefas', () => {
+  cy.visit('http://127.0.0.1:7001'); 
+
+  cy.get('.new-todo')
+    .type('TP2 de ES{enter}')
+    .type('Prova de ES{enter}');
+
+  cy.get('.toggle-all-label')
+    .first()
+    .click();
+  
+  cy.contains('Completed').click();
+  cy.get('.todo-list li')
+    .should('have.length', 2);
+
+  cy.get('.toggle-all-label')
+    .first()
+    .click();
+  
+  cy.contains('Active').click();
+    cy.get('.todo-list li')
+      .should('have.length', 2);
+ })
+
+ it('Criar e editar tarefa', () => {
+  cy.visit('http://127.0.0.1:7001');
+
+  cy.get('.new-todo')
+    .type('TP2 de EF{enter}');
+
+  cy.get('.todo-list li')
+    .first()
+    .dblclick();
+
+  cy.get('.todo-list li .edit')
+    .clear()
+    .type('TP2 de ES{enter}');
+
+  cy.get('.todo-list li')
+    .should('have.length', 1)
+    .first()
+    .should('have.text', 'TP2 de ES');
+});
+
 });
